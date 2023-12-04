fe01db438a9c31b7ffc915fdd2c116cc08d0b35c
ionut <ionutcatana1805@gmail.com>
2023-12-04 04:24:23 +0000

    Add project files.

---
+++ MainMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MainMethod
+    {
+        // First method that takes an integer
+        public int numResult(int number)
+        {
+            return number + 20;
+        }
+
+        // Second method that takes a decimal
+        public int numResult(decimal number)
+        {
+            // Converting the decimal in to a integer
+            int numInt = Convert.ToInt32(number);
+            // Performing a math operation with the converted decimal
+            return numInt * 1;
+        }
+
+        // Third method using a string
+        public int numResult(string number)
+        {
+            // converting the string in to a integer
+            int numInt = Convert.ToInt32(number);
+            return numInt % 24;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiate the MainMethod class
+            MainMethod result = new MainMethod();
+            // Calling first method that has a integer parameter
+            Console.WriteLine(result.numResult(18));
+            // Calinng second method that has a decimal parameter
+            Console.WriteLine(result.numResult(5.1m));
+            // Calling third method that has a string parameter
+            Console.WriteLine(result.numResult("1992"));
+            Console.ReadLine();
+        }
+        
+    }
+}
